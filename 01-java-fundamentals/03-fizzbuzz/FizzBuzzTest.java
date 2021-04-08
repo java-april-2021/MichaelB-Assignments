b8e23252e9d9f775104fd8c19cd42eee54171715
mbrowley24 <michael.a.browley@outlook.com>
2021-04-08 13:56:50 -0600

    Fizz Buzz completed

--- 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -1,10 +1,13 @@
 public class FizzBuzzTest{
     public static void main(String[] args){
         FizzBuzz testRun = new FizzBuzz();
-        testRun.fizzBuzz(3);
-        testRun.fizzBuzz(5);
-        testRun.fizzBuzz(15);
-        testRun.fizzBuzz(2);
-        
+        String str1 = testRun.fizzBuzz(3);
+        String str2 = testRun.fizzBuzz(5);
+        String str3 = testRun.fizzBuzz(15);
+        String str4 = testRun.fizzBuzz(2);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
     }
 }
