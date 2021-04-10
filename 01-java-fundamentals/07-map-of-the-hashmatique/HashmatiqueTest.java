e8a97448c1e8ef400e3be0b0c78822131327d256
mbrowley24 <michael.a.browley@outlook.com>
2021-04-10 14:21:19 -0600

    Hash map completed

---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/HashmatiqueTest.java
@@ -0,0 +1,249 @@
+import java.util.HashMap;
+public class HashmatiqueTest{
+    public static void main(String[] args){
+        String careless = "I feel so unsure"
+        .concat(" As I take your hand \n")
+        .concat("And lead you to the dance floor \n")
+        .concat("As the music dies \n")
+        .concat("Something in your eyes \n")
+        .concat("Calls to mind a silver screen \n")
+        .concat("And all its sad goodbyes \n")
+        
+        .concat("I'm never gonna dance again \n")
+        .concat("Guilty feet have got no rhythm\n")
+        .concat("Though it's easy to pretend \n")
+        .concat("I know you're not a fool \n")
+        .concat("I should've known better than to cheat a friend \n")
+        .concat("And waste the chance that I've been given \n")
+        .concat("So I'm never gonna dance again \n")
+        .concat("The way I danced with you \n")
+        
+        .concat("Time can never mend \n")
+        .concat("The careless whispers of a good friend \n")
+        .concat("To the heart and mind \n")
+        .concat("Ignorance is kind \n")
+        .concat("There's no comfort in the truth \n")
+        .concat("Pain is all you'll find \n")
+        
+        .concat("I'm never gonna dance again \n")
+        .concat("Guilty feet have got no rhythm \n")
+        .concat("Though it's easy to pretend \n")
+        .concat("I know you're not a fool \n")
+        .concat("I should've known better than to cheat a friend \n")
+        .concat("And waste the chance that I've been given \n")
+        .concat("So I'm never gonna dance again \n")
+        .concat("The way I danced with you \n")
+        
+        .concat("Tonight the music seems so loud \n")
+        .concat("I wish that we could lose this crowd \n")
+        .concat("Maybe it's better this way \n")
+        .concat("We'd hurt each other with the things we want to say \n")
+        .concat("We could have been so good together \n")
+        .concat("We could have lived this dance forever \n")
+        .concat("But now who's gonna dance with me \n")
+        .concat("Please stay \n")
+        
+        .concat("I'm never gonna dance again \n")
+        .concat("Guilty feet have got no rhythm \n")
+        .concat("Though it's easy to pretend \n")
+        .concat("I know you're not a fool \n")
+        .concat("I should've known better than to cheat a friend \n")
+        .concat("And waste the chance that I've been given \n")
+        .concat("So I'm never gonna dance again \n")
+        .concat("The way I danced with you \n")
+        
+        .concat("Now that you're gone \n")
+        .concat("Now that you're gone \n")
+        .concat("Now that you're gone \n")
+        .concat("Was what I did so wrong \n")
+        .concat("So wrong that you had to leave me alone? \n");
+
+        String faith = "Well I guess it would be nice \n"
+        +"If I could touch your body \n"
+        +"I know not everybody \n"
+        +"Has got a body like you \n"
+        
+        +"But I've got to think twice \n"
+        +"Before I give my heart away \n"
+        +"And I know all the games you play \n"
+        +"Because I play them too \n"
+        
+        +"Oh but I \n"
+        +"Need some time off from that emotion \n"
+        +"Time to pick my heart up off the floor \n"
+        +"Oh when that love comes down \n"
+        +"Without devotion \n"
+        +"Well it takes a strong man baby \n"
+        +"But I'm showing you the door \n"
+        
+        +"‘Cause I gotta have faith \n"
+        +"I gotta have faith \n"
+        +"‘Cause I gotta have faith, faith \n"
+        +"‘Cause I gotta have faith, faith, faith \n"
+        
+        +"Baby, \n"
+        +"I know you're asking me to stay \n"
+        +"Say please, please, please don't go away \n"
+        +"You say I'm giving you the blues \n"
+        +"Maybe \n"
+        +"You mean every word you say \n"
+        +"Can't help but think of yesterday \n"
+        +"And another who tied me down to loverboy rules \n"
+        
+        +"Before this river \n"
+        +"Becomes an ocean \n"
+        +"Before you throw my heart back on the floor \n"
+        +"Oh oh baby I reconsider \n"
+        +"My foolish notion \n"
+        +"Well I need someone to hold me \n"
+        +"But I'll wait for something more \n"
+        
+        +"Yes I've gotta have faith… \n"
+        +"Mmm, I gotta have faith \n"
+        +"‘Cause I gotta have faith, faith, faith \n"
+        +"I gotta have faith-a-faith-a-faith \n"
+        
+        +"Before this river \n"
+        +"Becomes an ocean \n"
+        +"Before you throw my heart back on the floor \n"
+        +"Oh oh baby I reconsider \n"
+        +"My foolish notion \n"
+        +"Well I need someone to hold me \n"
+        +"But I'll wait for something more \n"
+        
+        +"Cause I gotta have faith \n"
+        +"Mmm I gotta have faith \n"
+        +"Because I got to have faith-a-faith-a-faith \n"
+        +"I gotta have faith-a-faith-a-faith \n";
+
+        String as = "As Around The Sun The Earth Know She's Revolving"
+        + "And the rosebuds know to bloom in early may\n"
+        +"Just as hate knows love's the cure\n"
+        +"You can rest your mind assured that\n"
+        +"I'll be loving you always\n"
+        
+        +"As now can't reveal the mystery of tomorrow\n"
+        +"But in passing will grow older everyday\n"
+        +"Just as all that's born is new\n"
+        +"You know what I say is true\n"
+        +"That I'll be loving you always\n"
+        
+        +"Until the rainbow burns the stars out of the sky\n"
+        +"Until the ocean covers every mountain high\n"
+        +"Until the day that 8 times 8 times 8 is 4\n"
+        +"Until the day that is the day that are no more\n"
+        
+        +"Did you know that true love asks for nothing\n"
+        +"Her acceptance is the way we pay\n"
+        +"Did you know that life has given love a guarantee\n"
+        +"To last through forever and another day\n"
+        
+        +"As today I know I'm living but tomorrow\n"
+        +"Could make me the past\n"
+        +"But that I mustn't fear\n"
+        +"I know deep in my mind\n"
+        +"The love of me I've left behind\n"
+        +"Cause I'll be loving you always\n"
+        
+        +"Until the rainbow burns the stars out in the sky\n"
+        +"Until the ocean covers every mountain high\n"
+        +"Until the dolphin flies and parrots live at sea\n"
+        +"Until we dream of life and life becomes a dream\n"
+        +"Until the day is night and night becomes the day\n"
+        +"Until the trees and seas just up and fly away\n"
+        +"Until the day that 8 times 8 times 8 is 4\n"
+        +"Until the day that is the day that are no more\n"
+        
+        +"Did you know that true love asks for nothing?\n"
+        +"Her acceptance is the way we pay\n"
+        +"Did you know that life has given love a guarantee\n"
+        +"To last through forever and another day\n"
+        
+        +"As around the sun the earth knows she's revolving\n"
+        +"And the rosebuds know to bloom in early may\n"
+        +"For now I know deep in my mind\n"
+        +"The love of me I've left behind\n"
+        +"Cause I'll be loving you always\n"
+        
+        +"Until the rainbow burns the stars out in the sky\n"
+        +"Until the ocean covers every mountain high\n"
+        +"Until the dolphin flies and parrots live at sea\n"
+        +"Until we dream of life and life becomes a dream\n"
+        
+        +"Until the day is night and night becomes the day\n"
+        +"Until the trees and seas just up and fly away\n"
+        +"Until the day that 8 times 8 time 8 is 4\n"
+        +"Until the day that is the day that are no more\n";
+
+        String if_you_were_my_woman = "If you were my woman\n"
+        +"And I was your man\n"
+        +"There would be no other woman\n"
+        +"You'd be weak as a lamb\n"
+        +"If you had the strength to walk out that door\n"
+        +"My love would overrule my sense I would call you back for more\n"
+        
+        +"If you were my woman\n"
+        +"If you were my woman\n"
+        +"And I was your man, Yeah!\n"
+        
+        +"He tears you down darlin'\n"
+        +"Says your nothing at all\n"
+        +"But I'll be there for darlin'\n"
+        +"When he lets fall\n"
+        +"Your like a diamond\n"
+        +"But he treats you like glass\n"
+        +"Yet you beg him to love you\n"
+        +"With me you don't ask\n"
+        
+        +"If you were my woman\n"
+        +"If you were my woman\n"
+        +"Here's what I'd do, Oh\n"
+        +"I'd never, no, no, no stop lovin' you\n"
+        +"Whoa! Yeah!\n"
+        
+        +"My darlin'\n"
+        +"Life is so crazy\n"
+        +"And love is so unkind\n"
+        +"Because he came first, darlin'\n"
+        +"Will he make up your mind\n"
+        +"Your a part of me\n"
+        +"And you don't even to know it\n"
+        +"I'm what you need\n"
+        +"But I'm too afraid to show it\n"
+        
+        +"If you were my woman\n"
+        +"Here's what I'd do, Oh\n"
+        +"I'd never, no, no, no\n"
+        +"Whoa! Oh!\n"
+        
+        +"My darlin' If you were my woman\n"
+        +"Sweet lovin' woman\n"
+        +"I said\n"
+        +"If you were my woman\n"
+        +"If you were my woman\n"
+        +"I said\n"
+        +"If you were my woman\n"
+        +"If you were my woman\n"
+        +"Here's what I would do, Oh\n"
+        +"I'd never, no, no, no stop lovin' you\n"
+        +"Whoa! Yeah!\n"
+        +"Never, never, no\n"
+        +"Hmm...\n";
+        
+
+        
+        HashMap<String, String> songLyrics = new HashMap<String, String>();
+        songLyrics.put("Careless Whisper", careless);
+        songLyrics.put("Faith", faith);
+        songLyrics.put("As", as);
+        // display pyrics for two songs
+        songLyrics.put("If You Were My Woman", if_you_were_my_woman);
+        System.out.println(songLyrics.get("Careless Whisper"));
+        System.out.println(songLyrics.get("Faith"));
+
+        for(HashMap.Entry<String, String> entry : songLyrics.entrySet()){
+            System.out.println("Song title: "  + entry.getKey() + "\nlyrics:\n" + entry.getValue());
+        }
+
+    }
+}
