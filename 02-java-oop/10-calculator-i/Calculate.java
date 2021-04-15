5acae1a443f1b75a344be23d3f1457b49904ca5a
mbrowley24 <michael.a.browley@outlook.com>
2021-04-15 00:35:09 -0600

    calculator completed

---
+++ 02-java-oop/10-calculator-i/Calculate.java
@@ -0,0 +1,15 @@
+public class Calculate {
+    double value;
+    String operator;
+    
+    public Calculate(){
+
+    }
+    public Calculate(double value, String operator){
+        this.value = value;
+        this.operator = operator;
+    }
+
+
+
+}
